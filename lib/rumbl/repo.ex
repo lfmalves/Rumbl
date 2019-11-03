ad3f0509c18dcb48174713989d13a2d4a7ba5d02
Luis F M Alves <luis.projensino@gmail.com>
2019-11-03 13:39:53 -0300

    feat: dyadic function get_by for Repo

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -13,4 +13,9 @@ defmodule Rumbl.Repo do
   def get(module, id) do
   	Enum.find all(module), fn map -> map.id == id end
   end
+
+  def get_by(module, params) do
+  	Enum.find all(module), fn map ->
+  		Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+  end
 end
